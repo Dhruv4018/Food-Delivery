4a748777ce738b1c0bbf6457bd408804b4544b0b
DHRUV JAIN <dj4018386@gmail.com>
2025-10-30 18:21:12 +0530

    update url

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors:{
-    origin: "http://localhost:5173",
+    origin: "https://food-delivery-v7xl.onrender.com",
     credentials: true,
     methods:["POST","GET"]
 
@@ -34,7 +34,7 @@ app.set("io",io)
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://food-delivery-v7xl.onrender.com",
     credentials: true
 
 }))
@@ -55,4 +55,5 @@ app.get("/", (req, res) => {
 server.listen(port, () => {
     connectDB()
     console.log("server is running ", port);
+
 })
